4c5890b2340df813ba60ea8fd837e0ea040e604a
Aldair Roque <ronaldrv886@gmail.com>
2026-07-02 02:09:40 +0000

    cambio

---
+++ examen-pc3/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="container">
+
+      <h1 className="titulo">PC3 - React</h1>
+
+      <div className="pregunta">
+        <h2>Pregunta 1</h2>
+
+        <p>
+          Aquí irá la pregunta del examen.
+        </p>
+
+        {/* Desarrollo de la solución */}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
